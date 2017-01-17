ae1be4c20981c7d02272736fade2b2b7b76ae0b4
Michael Douglas <mdcs@br.ibm.com>
2017-01-17 01:02:49 -0200

    - more adjusts

--- iHungry/InterfaceExtensions.swift
+++ iHungry/InterfaceExtensions.swift
@@ -1,8 +1,8 @@
 //
-//  LoginViewController.swift
+//  InterfaceExtensions.swift
 //  iHungry
 //
-//  Created by Michael Douglas on 15/01/17.
+//  Created by Michael Douglas on 17/01/17.
 //  Copyright © 2017 Michael Douglas. All rights reserved.
 //
 
@@ -26,60 +26,39 @@ import UIKit
 //
 //**************************************************************************************************
 
-class LoginViewController: UIViewController, UITextFieldDelegate {
-
 //*************************************************
 // MARK: - Properties
 //*************************************************
-    
-    @IBOutlet weak var scrollView: UIScrollView!
-    @IBOutlet weak var userTextField: UITextField!
-    @IBOutlet weak var passwordTextField: UITextField!
-    
-//*************************************************
-// MARK: - Override Public Methods
-//*************************************************
-    
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        self.hideKeyboardWhenTappedAround()
 
-    }
-    
 //*************************************************
 // MARK: - Constructors
 //*************************************************
-    
+
 //*************************************************
 // MARK: - Private Methods
 //*************************************************
-    
+
 //*************************************************
 // MARK: - Internal Methods
 //*************************************************
-    
-    @IBAction func signIn(_ sender: RoundedButton) {
-        
-    }
 
-    
 //*************************************************
 // MARK: - Self Public Methods
 //*************************************************
 
-    func textFieldDidBeginEditing(_ textField: UITextField) {
-        if (textField == userTextField || textField == passwordTextField) {
-            scrollView.setContentOffset(CGPoint.init(x: 0, y: 100), animated: true)
-        }
+//**************************************************************************************************
+//
+// MARK: - Extension -
+//
+//**************************************************************************************************
+
+extension UIViewController {
+    func hideKeyboardWhenTappedAround() {
+        let tap: UITapGestureRecognizer = UITapGestureRecognizer(target: self, action: #selector(UIViewController.dismissKeyboard))
+        view.addGestureRecognizer(tap)
     }
     
-    func textFieldDidEndEditing(_ textField: UITextField) {
-        scrollView.setContentOffset(CGPoint.init(x: 0, y: 0), animated: true)
+    func dismissKeyboard() {
+        view.endEditing(true)
     }
-    
-    func textFieldShouldReturn(_ textField: UITextField) -> Bool {
-        textField.resignFirstResponder()
-        return true
-    }
-    
 }
